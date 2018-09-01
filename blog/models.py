6b3bab066051fc9bda370615e70e2a44fc4d29d5
sagebeach <sagebeach@protonmail.com>
2018-08-31 19:57:17 -0700

    redone styling. Refactored main page into Django template models. Added summary onto post_list.html in model.py with migration.

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 class Post(models.Model):
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
+	summary = models.TextField(default="lets go!")
 	text = models.TextField()
 	created_date = models.DateTimeField(
 		default=timezone.now)
--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'blog',
 ]
@@ -48,9 +49,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+
+TEMPLATE_LOADERS = (  
+    ('django.template.loaders.cached.Loader', (  
+        'django.template.loaders.filesystem.Loader',  
+        'django.template.loaders.app_directories.Loader',  
+    )),  
+)  
 
 TEMPLATES = [
     {
